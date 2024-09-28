a5e736042c57c4685256263da57449f0b25d1f7d
Josh P <joshpellows@gmail.com>
2024-09-28 07:23:27 -0500

    initial commit

--- LearningTradingStrat.py
+++ LearningTradingStrat.py
@@ -1,1 +1,21 @@
 # Unsupervised Learning Trading Strategy
+from statsmodels.regression.rolling import RollingOLS
+import pandas_datareader.data as web
+import matplotlib.pyplot as plt
+import statsmodels.api as sm
+import pandas as pd
+import numpy as np
+import datetime as dt
+import yfinance as yf
+import pandas_ta
+import warnings
+warnings.filterwarnings('ignore')
+sp500 = pd.read_html('https://en.wikipedia.org/wiki/List_of_S%26P_500_companies')[0]
+sp500['Symbol'] = sp500['Symbol'].str.replace(".","-")
+symbols_list = sp500['Symbol'].unique().tolist() #not survivorship bias free
+end_date = '2023-09-27'
+
+start_date = pd.to_datetime(end_date)-pd.DateOffset(365*8)
+df = yf.download(tickers=symbols_list,start=start_date,end=end_date)
+
+df.stack()
